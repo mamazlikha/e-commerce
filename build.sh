f29c40986d679e046981a34fdc2b19b96e31a74a
Anas <anas.francis@etu.univ-cotedazur.fr>
2024-09-13 00:08:26 +0200

    remove cat secrets

--- build.sh
+++ build.sh
@@ -5,8 +5,22 @@ set -e
 maven_username=$1
 maven_token=$2
 
-
-cd userservice
+cd ProductsService
 mvn test
-echo $1 $2
+docker build -t productservice .
+
+cd ../CartService
+mvn test
+docker build -t cartservice .
+
+cd ../inventoryservice
+mvn test
+docker build -t inventoryservice .
+
+cd ../ProductsService
+mvn test
+docker build -t productsservice .
+
+cd ../userservice
+mvn test
 docker build --build-arg MAVEN_USERNAME=$1 --build-arg MAVEN_TOKEN=$2 -t userservice .
