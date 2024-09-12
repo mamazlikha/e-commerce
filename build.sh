3c644c1e82193332e3a8b8240f5ea29dfff37773
Anas <anas.francis@etu.univ-cotedazur.fr>
2024-09-13 00:54:33 +0200

    debug with messages

--- build.sh
+++ build.sh
@@ -2,26 +2,11 @@
 
 set -e
 
-echo "========== Building productservice microservice ========"
-cd ProductsService
-mvn test
-docker build -t productservice .
-
-echo "========== Building cartservice microservice ========"
-cd ../CartService
-mvn test
-docker build -t cartservice .
-
-echo "========== Building inventoryservice microservice ========"
-cd ../inventoryservice
-mvn test
-docker build -t inventoryservice .
-
 echo "========== Building userservice microservice ========"
-cd ../userservice
+echo $1 $2
+cd userservice
 mvn test
 docker build --build-arg MAVEN_USERNAME=$1 --build-arg MAVEN_TOKEN=$2 -t userservice .
 
-echo $1 $2
 echo "========== Build eneded ! ========"
 
