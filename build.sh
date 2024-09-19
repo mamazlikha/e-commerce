b33583b6ce9cb4398c3bb24f0ed2eb454c98e5b6
Anas <anas.francis@etu.univ-cotedazur.fr>
2024-09-19 22:21:45 +0200

    remove duplicates in pom.xml

--- build.sh
+++ build.sh
@@ -2,11 +2,15 @@
 
 set -e
 
-echo "========== Building userservice microservice ========"
+echo "========== Building userservice microservice ========\n"
 echo $1 $2
 cd userservice
-mvn test
+echo "========== Running tests ========\n"
+export MAVEN_USERNAME=$1
+export MAVEN_TOKEN=$2
+mvn test -s ./settings.xml
+echo "========== Building docker image ! ========\n"
 docker build --build-arg MAVEN_USERNAME=$1 --build-arg MAVEN_TOKEN=$2 -t userservice .
 
-echo "========== Build eneded ! ========"
+echo "========== Build ended ! ========"
 
